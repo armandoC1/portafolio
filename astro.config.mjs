d81232aff62e08c4f020dbc4f92ac8e16ede08b6
Carlos Hernández <ch148640@gmail.com>
2025-10-05 16:38:25 -0600

    add: dependence failed to production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
+// astro.config.mjs
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
-import { defineConfig, squooshImageService } from 'astro/config';
+import icon from 'astro-icon';
+import { defineConfig, sharpImageService } from 'astro/config'; 
 
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon({
-    include: {
-      mdi: ["*"]
-    }
-  })],
+  integrations: [
+    tailwind(),
+    mdx(),
+    icon({ include: { mdi: ['*'] } })
+  ],
+  image: { service: sharpImageService() } 
 });
